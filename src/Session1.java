07a160355007fd2962da2b91dabc7295705f3bcd
aminzadeh.majid@gmail.com <aminzadeh.majid@gmail.com>
2021-12-16 22:53:30 +0330

    init commit

---
+++ src/Session1.java
@@ -0,0 +1,40 @@
+public class Session1 {
+    long val;
+    long Val;
+    int مجید;
+    int p012123123123;
+    int _ق;
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int amount;
+        amount = 23;
+        System.out.println(amount);
+        long longAmount;
+        longAmount = -230000000000000l;
+        System.out.println(longAmount);
+        float floatAmount;
+        floatAmount = 23.9f;
+        System.out.println(floatAmount);
+        double doubleAmount;
+        doubleAmount = 23.9;
+        System.out.println(doubleAmount);
+        char chars;
+        chars = 'C';
+        System.out.println(chars);
+        char chars2 = 67;
+        System.out.println(chars2);
+        int intChar = 'C';
+        System.out.println(intChar);
+        String value;
+        value = "Tosan Academy";
+        System.out.println(value);
+        int val = 1;
+        System.out.println(val);
+        boolean b;
+        b = true;
+        b = false;
+        int a = 1;
+        int c = 2;
+        System.out.println(a=c);
+    }
+}
